1883a82fd80c5f5e28526af51ddb55f687278d00
Kyle Long <kylel95@vt.edu>
2020-07-12 19:52:48 -0400

    Most updated version. Handling error checking

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Axios from "axios";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
-import ListGroup from "react-bootstrap/ListGroup";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
 function App() {
   let initialWords = JSON.parse(localStorage.getItem("words")) || [];
   const [vocab, setVocab] = useState("");
@@ -19,75 +10,126 @@ function App() {
   const [example, setExample] = useState("");
   const [load, setLoad] = useState(false);
   const [words, setWords] = useState(initialWords);
+  const [exist, setExist] = useState(false);
+  const [err, setErr] = useState(false);
   function addToList() {
-    setWords((prevArray) => [...prevArray, word]);
+    setExist(false);
+    let list = words;
+    if (!list.includes(word)) {
+      setWords((prevArray) => [...prevArray, word]);
+    } else {
+      setExist(true);
+    }
+  }
+  function viewWord(e) {
+    setWord(e.target.innerText);
   }
   useEffect(() => {
+    setExist(false);
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     Axios.get(url)
       .then((response) => {
         setLoad(true);
+        setErr(false);
         setWord(response.data[0].word);
         setSpeech(response.data[0].meanings[0].partOfSpeech);
         setDefinition(response.data[0].meanings[0].definitions[0].definition);
         setExample(response.data[0].meanings[0].definitions[0].example);
       })
-      .catch((error) => console.log());
+      .catch((error) => {
+        setErr(true);
+        setWord("");
+        setSpeech("");
+        setDefinition("");
+        setExample("");
+      });
     let list = words;
     localStorage.setItem("words", JSON.stringify(list));
-  }, [word, words]);
+  }, [word, words, err]);
 
   return (
     <>
-      <h2 className="title">Words</h2>
+      <div id="header">
+        <h2 className="title">Words</h2>
+        <p>Track the words you learn.</p>
+        <input
+          onChange={(e) => setVocab(e.target.value)}
+          type="text"
+          placeholder="Enter a word"
+          id="input"
+        />
 
-      <Container className="d-flex justify-content-center">
-        <Row className="text-center">
-          {" "}
-          <Form>
-            <Form.Group className="align-items-center">
-              <Col>
-                <Form.Control
-                  onChange={(e) => setVocab(e.target.value)}
-                  type="text"
-                  placeholder="Enter a word"
-                  className="mx-sm-3"
-                  id="input"
-                />
-              </Col>
-            </Form.Group>
-            <Button onClick={() => setWord(vocab)} variant="outline-primary">
-              Search
-            </Button>
-          </Form>
-        </Row>
-      </Container>
+        <button className="button" onClick={() => setWord(vocab)}>
+          SEARCH
+        </button>
+      </div>
 
       {load && (
-        <Container className=" mt-5 d-flex justify-content-center">
-          <Card
-            className="h-100 shadow-sm bg-white rounded"
-            style={{ width: "18rem" }}
-          >
-            <Card.Header></Card.Header>
-            <Card.Body>
-              <Card.Title>{word}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                {speech}
-              </Card.Subtitle>
-              <Card.Text>{definition}</Card.Text>
-              {example && <Card.Text>"{example}"</Card.Text>}
-              <Button
-                className="mt-auto font-weight-bold"
-                variant="outline-success"
-                onClick={() => addToList()}
-                block
+        <div id="wordBox">
+          <h3>{word}</h3>
+          <p>{speech}</p>
+          <p>{definition}</p>
+          {example && <p>"{example}"</p>}
+          <button className="button" onClick={() => addToList()}>
+            ADD
+          </button>
+          {/*The word already exist */}
+          {exist && (
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-alert-triangle"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="#FFC107"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
-                Add
-              </Button>
-            </Card.Body>
-          </Card>
-        </Container>
+                <path stroke="none" d="M0 0h24v24H0z" />
+                <path d="M12 9v2m0 4v.01" />
+                <path d="M5.07 19H19a2 2 0 0 0 1.75 -2.75L13.75 4a2 2 0 0 0 -3.5 0L3.25 16.25a2 2 0 0 0 1.75 2.75" />
+              </svg>
+              {word} is already saved.
+            </div>
+          )}
+
+          {err && (
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-alert-triangle"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="#FFC107"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" />
+                <path d="M12 9v2m0 4v.01" />
+                <path d="M5.07 19H19a2 2 0 0 0 1.75 -2.75L13.75 4a2 2 0 0 0 -3.5 0L3.25 16.25a2 2 0 0 0 1.75 2.75" />
+              </svg>
+              The word you entered is invalid.
+            </div>
+          )}
+        </div>
+      )}
+      {words.length > 0 && (
+        <div id="learned">
+          <h3>Learned Words</h3>
+          <ul>
+            {words.map((word, index) => (
+              <li key={index} onClick={(e) => viewWord(e)}>
+                {word}{" "}
+              </li>
+            ))}
+          </ul>
+        </div>
       )}
     </>
   );
